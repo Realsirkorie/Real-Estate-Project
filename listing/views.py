4a59d7b9bdd7295a889bd96e12dd2a2d02868a8d
Okorie Oluchukwu Rufus <realsirkorie@gmail.com>
2023-06-15 20:49:33 +0100

    first commit

---
+++ listing/views.py
@@ -0,0 +1,222 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response  
+from rest_framework import status, permissions
+from .models import Listing
+from .serializer import ListingSerializer
+
+class ManageListingView(APIView):
+    def get(self, request, format=None):
+        try:
+            user = request.user
+            
+            if not user.is_realtor:
+                return Response(
+                    {'error': 'User does not have neccessary permissions for getting this listing data'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+                
+            slug = request.query_params.get('slug')
+            
+            if not slug:
+                listing = Listing.objects.order_by('-date_created').filter(
+                    realtor=user.email
+                )
+                listing = ListingSerializer(listing, many=True)
+                
+                return Response(
+                    {'listing': listing.data},
+                    status=status.HTTP_200_OK
+                )
+            
+            if not Listing.objects.filter(
+                realtor=user.email,
+                slug=slug
+            ).exists():
+                return Response(
+                    {'error': 'Listing not found'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            listing = listing.objects.get(realtor=user.email, slug=slug)
+            listing = ListingSerializer(listing)
+            
+            return Response(
+                {'listing': listing.data},
+                status=status.HTTP_200_OK
+            )
+            
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving listing or listing detail'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    
+    def post(self, request):
+        try:
+            user = request.user 
+            
+            if not user.is_realtor:
+                return Response(
+                    {'error': 'User does ot have neecessary Permissions for creating this listing data'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            
+            data = request.data
+            
+            title = data['title']
+            slug = data['slug']
+            
+            if Listing.objects.filter(slug=slug).exists():
+                return Response(
+                    {'error': 'Listing with this slug already exists'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+                
+            address = data['address']
+            city = data['city']
+            state = data['state']
+            zipcode = data['zipcode']
+            description = data['description']
+            
+            price = data['price']
+            try:
+                price = int(price)
+            except:
+                return Response(
+                    {'error': 'Price must be an integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            bedrooms = data['bedrooms']
+            try: 
+                bedrooms = int(bedrooms)
+            except: 
+                return Response(
+                    {'error': 'Bedrooms must be an integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+                
+            bathrooms = data['bathrooms']
+            try: 
+                bathrooms = float(bathrooms)
+            except: 
+                return Response(
+                     {'error': 'Bathrooms must be a floating point value'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            if bathrooms <= 0 or bathrooms >= 10:
+                bathrooms = 1.0
+            
+            bathrooms = round(bathrooms, 1)
+            
+            sale_type = data['sale_type']
+            
+            if sale_type == 'FOR_RENT':
+                sale_type = 'For Rent'
+            else:
+                sale_type = 'For Sale'
+                
+            home_type = data['home_type']
+            
+            if home_type == 'CONDO':
+                home_type = 'Condo'
+            elif home_type == 'TOWNHOUSE':
+                home_type = 'Townouse'
+            else:
+                home_type = 'House'
+            
+            main_photo = data['main_photo']
+            photo_1 = data['photo_1']
+            photo_2 = data['photo_2']
+            photo_3 = data['photo_3']
+            is_published = data['is_published']
+            
+            if is_published == 'True':
+                is_published = True
+            else: 
+                is_published = False
+            
+            Listing.objects.create(
+                realtor=user.email,
+                title=title,
+                slug=slug,
+                address=address,
+                city=city,
+                state=state,
+                zipcode=zipcode,
+                description=description,
+                price=price,
+                bedrooms=bedrooms,
+                bathrooms=bathrooms,
+                sale_type=sale_type,
+                home_type=home_type,
+                main_photo=main_photo,
+                photo_1=photo_1,
+                photo_2=photo_2,
+                photo_3=photo_3,
+                is_published=is_published
+            )
+            
+            return Response(
+                 {'success': 'Listing created successfully'},
+                 status=status.HTTP_201_CREATED
+            )
+        except:
+            return Response(
+                {'error': 'Somethting went wrong when creating listing'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+class ListingDetailView(APIView):
+    def get(self, request, format=None):
+        try:
+            slug = request.query_params.get('slug')
+            
+            if not slug:
+                return Response(
+                    {'error': 'Must provide slug'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            if not Listing.objects.filter(slug=slug, is_published=True).exists():
+                return Response(
+                    {'error': 'Published listing with this slug does not exist'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+                
+            listing = Listing.objects.get(slug=slug, is_published=True)
+            listing =ListingSerializer(listing)
+            
+            return Response(
+                {'listing': listing.data},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving detail'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+class ListingsView(APIView):
+    permission_classes = (permissions.AllowAny, )
+    
+    def get(self, request, format=None):
+        try:
+            if not Listing.objects.filter(is_published=True).exists():
+                return Response(
+                    {'error': 'No published listings in the database'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            
+            listings = Listing.objects.order_by('-date_created').filter(is_published=True)
+            listings = ListingSerializer(listings, many=True)
+            
+            return Response(
+                {'listings': listings.data},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving istings'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
