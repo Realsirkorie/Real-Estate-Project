71eec7a7458208c4d54ac2f1ee0727f4c189de1d
Okorie Oluchukwu Rufus <realsirkorie@gmail.com>
2023-06-16 17:04:29 +0100

    First commit

--- listing/urls.py
+++ listing/urls.py
@@ -4,5 +4,5 @@ from .views import ManageListingView, ListingDetailView, ListingsView
 urlpatterns = [
     path('manage', ManageListingView.as_view()), 
     path ('detail', ListingDetailView.as_view()),
-    path('get-listings', ListingsView.as_view()), 
+    path('get-listings', ListingsView.as_view()),
 ]
